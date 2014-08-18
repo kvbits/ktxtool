57ff32d2ad030b7a7395ed3f9b4e44f4238bade8
Luis Jimenez <kuko@kvbits.com>
2014-08-18 16:01:31 -0500

    added gpl license to each file

--- source/ktxtool.cpp
+++ source/ktxtool.cpp
@@ -1,8 +1,28 @@
+/*
+ * ktxtool, A convertion/compression tool for the KTX image format
+ *
+ * Copyright (C) 2014 Luis Jimenez, www.kvbits.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
+ */
+
 #include <iostream>
 #include <map>
 #include <string>
 #include <assert.h>
-
+#include <iomanip> 
 
 
 
@@ -89,6 +109,50 @@ Option& AddOption(char id, int flags, const char* desc)
 
 
 
+static void DumpOptions()
+{
+	using namespace std;
+
+	OptionMap::iterator it = options.begin();
+
+	cout << "  Options:" << endl;
+
+	while (it != options.end())
+	{
+		Option& opt = it->second;
+
+		string flags = "";
+
+		if (opt.IsDefined())
+		{
+			flags += "OPTION_DEFINED ";
+		}
+
+		if (opt.ExpectsValue())
+		{
+			flags += "OPTION_EXPECTS_VALUE ";
+		}
+
+		if (opt.IsRequired())
+		{
+			flags += "OPTION_REQUIRED ";
+		}
+
+		cout << "    -" << opt.id << " ";
+		
+		//cout << setfill(' ') << setw(15) << opt.value << " : " << flags << endl;
+		cout << setfill(' ') << setw(15) << "  :  ";
+		cout << opt.desc;
+
+		cout << endl;
+
+		it++;
+	}
+
+}
+
+
+
 int main (int argc, char* argv[])
 {
 	using namespace std;
@@ -97,12 +161,18 @@ int main (int argc, char* argv[])
 	//define the options
 	AddOption('c', OPTION_EXPECTS_VALUE, "Use a compression format [ETC1, ETC2, PTVPR, ] (Currently not supported)");
 	AddOption('v', 0, "Verbose output");
+	AddOption('f', OPTION_REQUIRED | OPTION_EXPECTS_VALUE, "Input file");
+	AddOption('o', OPTION_EXPECTS_VALUE, "Output file");
 
 
 	if (argc < 2)
 	{
 		cout << "ktxtool v0.1.0" << endl << endl;
-		cout <<  "Usage: ktxtool -[OPTIONS]... FILEIN [FILEOUT]" << endl << endl;
+		cout << "  Usage: ktxtool -[OPTIONS]... FILEIN [FILEOUT]" << endl << endl;
+		
+		DumpOptions();
+
+		cout << endl << endl;
 
 		return 1;
 	}
@@ -180,6 +250,7 @@ int main (int argc, char* argv[])
 	{
 		assert(opt != NULL);
 		cerr << "Option -" << opt->id << " expected a value!" << endl;
+		return 4;
 	}
 
 	//now parse some utility arguments (the last 2) as FILEIN FILEOUT
@@ -190,39 +261,9 @@ int main (int argc, char* argv[])
 
 	//cout << (argc - parsedArg) << endl;
 
-#ifdef _DEBUG
+
 	
-	OptionMap::iterator it = options.begin();
 
-	cout << "Option DUMP:" << endl;
-
-	while (it != options.end())
-	{
-		Option& opt = it->second;
-
-		string flags = "";
-
-		if (opt.IsDefined())
-		{
-			flags += "OPTION_DEFINED ";
-		}
-
-		if (opt.ExpectsValue())
-		{
-			flags += "OPTION_EXPECTS_VALUE ";
-		}
-
-		if (opt.IsRequired())
-		{
-			flags += "OPTION_REQUIRED ";
-		}
-
-		cout << " -" << opt.id << " " << opt.value << " : " << flags << endl;
-
-		it++;
-	}
-	
-#endif
 	
 
 
